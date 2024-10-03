02e762928d0a26d7bf50eaf90584b4df9c3c1eee
JoaoPatrickMartins <joaopatrick@digitcontrol.com.br>
2024-10-03 09:31:12 -0300

    feat: Implement automated ticket query and status update functionality

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import pyautogui
+import time
+import pyperclip
+
 
 # Função para imprimir o valor
 def print_valor(data, valor):
@@ -26,9 +30,178 @@ if df.shape[1] < 4:
 # Dropar as 5 primeiras linhas
 df = df.drop(range(5))
 
-# Varre a planilha linha a linha e obtém o valor da quarta coluna
+# adicionar nome as colunas e uma coluna adicional chamada status
+df.columns = ['Data Venda', 'Pax', 'Form', 'Nr. Doc', 'Rota Resumida', 'Total Tarifa']			
+
+df['Status'] = ''
+
+# Varre a planilha linha a linha e obtém o valor da quarta coluna 
+# print temporário do dataframe
 for index, row in df.iterrows():
+    form = row.iloc[2]
     valor = row.iloc[3]  # iloc[3] refere-se à quarta coluna
     data = row.iloc[0]  # iloc[0] refere-se à primeira coluna
-    
-    print_valor(data, valor)    
+
+    localizador = form + valor
+    print_valor(data, localizador)    
+
+# iniciar acesso ao sistema
+# Pausar entre as ações para dar tempo ao sistema de responder
+pyautogui.PAUSE = 1
+
+# Minimizar todas as janelas e ir para a área de trabalho (atalho Win + D)
+pyautogui.hotkey('win', 'd')
+
+# Adicione uma pausa para garantir que o sistema tenha tempo de responder
+time.sleep(1)
+
+# Clicar no ícone do programa na área de trabalho (ajuste as coordenadas conforme necessário)
+pyautogui.click(x= 1851, y= 37)  # Coordenadas do programa
+
+# Digitar 1 enter
+pyautogui.press('enter')
+
+# Adicione uma pausa para garantir que o sistema tenha tempo de responder
+time.sleep(5)
+
+# Clicar no checkbox  e executar
+# Digitar 2 tabs
+pyautogui.press('tab')
+pyautogui.press('tab')
+
+# Digitar espaço
+pyautogui.press('space')
+
+# Digitar 1 enter
+pyautogui.press('enter')
+
+# Clicar no checkbox  e executar
+# Digitar 2 tabs
+pyautogui.press('tab')
+pyautogui.press('tab')
+
+# Digitar espaço
+pyautogui.press('space')
+
+# Digitar 1 enter
+pyautogui.press('enter')
+
+#login
+# Adicione uma pausa para garantir que o sistema tenha tempo de responder
+# Ajustar para 30 segundos para garantir que o sistema tenha tempo de responder
+time.sleep(10)
+# Digitar o usuário
+pyautogui.write('T063971')
+
+# Digitar 1 tab
+pyautogui.press('tab')
+
+# Digitar a senha
+pyautogui.write('12345678')
+
+# Digitar 1 tab
+pyautogui.press('tab')
+
+# Digitar 1 enter
+pyautogui.press('enter')
+
+# selecionar viacao
+# Adicione uma pausa para garantir que o sistema tenha tempo de responder
+# Ajustar para 30 segundos para garantir que o sistema tenha tempo de responder
+time.sleep(10)
+
+# Digitar 2 tabs
+pyautogui.press('tab')
+pyautogui.press('tab')
+
+# Digitar 1 seta para baixo
+pyautogui.press('down')
+
+# Digitar 1 tab
+pyautogui.press('tab')
+
+# Digitar 1 enter
+pyautogui.press('enter')
+
+#confirmar msg de aguardar para acesso
+# Digitar 1 enter
+pyautogui.press('enter')
+
+#abrir menu de consulta de bilhetes
+# Adicione uma pausa para garantir que o sistema tenha tempo de responder
+# Ajustar pra 60 segundos para garantir que o sistema tenha tempo de responder
+time.sleep(30)
+
+# Varre a planilha linha a linha e obtém o valor da quarta coluna
+for index, row in df.iterrows():
+    form = row.iloc[2]
+    valor = row.iloc[3]  # iloc[3] refere-se à quarta coluna
+    data = row.iloc[0]  # iloc[0] refere-se à primeira coluna
+
+    localizador = form + valor
+
+    # Clicar na caixa (ajuste as coordenadas conforme necessário)
+    pyautogui.click(x=1767, y=478)
+    pyautogui.click(x=19, y=34)
+
+    # Digitar "b"
+    pyautogui.write('n')
+
+    # parte com loop das consultas
+    # Adicione uma pausa para garantir que o sistema tenha tempo de responder
+    time.sleep(2)
+    # # Digitar 2 tabs
+    pyautogui.press('tab')
+    pyautogui.press('tab')
+    pyautogui.press('tab')
+
+    # Digitar o número do localizador
+    # OUFBERGG localizador existente no sistema
+    # GUFBEXZG localizador inexistente no sistema
+    pyautogui.write(localizador)
+
+    # Pressionar F12
+    pyautogui.press('f12')
+
+    # Adicione uma pausa para garantir que o sistema tenha tempo de responder
+    # Ajustar para 5 segundos para garantir que o sistema tenha tempo de responder
+    time.sleep(1)
+
+    # Pressionar F5
+    pyautogui.press('f5')
+
+    # Pressionar control + a
+    pyautogui.hotkey('ctrl', 'a')
+
+
+    # Limpar a área de transferência
+    pyperclip.copy('')
+
+    # Adicione uma pausa para garantir que a área de transferência esteja limpa
+    time.sleep(1)
+
+    # Copiar o texto selecionado para a área de transferência
+    pyautogui.hotkey('ctrl', 'c')
+
+    # Adicione uma pausa para garantir que o sistema tenha tempo de copiar o texto
+    time.sleep(1)
+
+    # Obter o texto da área de transferência
+    selected_text = pyperclip.paste()
+
+    # Verificar se há texto selecionado
+    if selected_text:
+        # adicionar status ao dataframe
+        df.loc[index, 'Status'] = 'inexistente'
+
+        # Pressionar F5
+        pyautogui.press('f5')
+    else:
+        # adicionar status ao dataframe
+        df.loc[index, 'Status'] = 'existente'
+
+# Print temporário do DataFrame
+print(df)
+
+# Salvar o DataFrame em um novo arquivo Excel
+df.to_excel(r'C:\Users\joaop\Joao Patrick\Trabalho\Digit Control\Program\analise-passagens\resultado.xlsx', index=False)
--- teste.py
+++ teste.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+import pyperclip
 
 # Pausar entre as ações para dar tempo ao sistema de responder
 pyautogui.PAUSE = 1
@@ -43,7 +44,8 @@ pyautogui.press('enter')
 
 #login
 # Adicione uma pausa para garantir que o sistema tenha tempo de responder
-time.sleep(5)
+# Ajustar para 30 segundos para garantir que o sistema tenha tempo de responder
+time.sleep(10)
 # Digitar o usuário
 pyautogui.write('T063971')
 
@@ -61,7 +63,8 @@ pyautogui.press('enter')
 
 # selecionar viacao
 # Adicione uma pausa para garantir que o sistema tenha tempo de responder
-time.sleep(5)
+# Ajustar para 30 segundos para garantir que o sistema tenha tempo de responder
+time.sleep(10)
 
 # Digitar 2 tabs
 pyautogui.press('tab')
@@ -82,14 +85,15 @@ pyautogui.press('enter')
 
 #abrir menu de consulta de bilhetes
 # Adicione uma pausa para garantir que o sistema tenha tempo de responder
-time.sleep(10)
+# Ajustar pra 60 segundos para garantir que o sistema tenha tempo de responder
+time.sleep(30)
 
 # Clicar na caixa (ajuste as coordenadas conforme necessário)
 pyautogui.click(x=1767, y=478)
-pyautogui.click(x=478, y=34)
+pyautogui.click(x=19, y=34)
 
 # Digitar "b"
-pyautogui.write('b')
+pyautogui.write('n')
 
 # parte com loop das consultas
 # Adicione uma pausa para garantir que o sistema tenha tempo de responder
@@ -97,9 +101,42 @@ time.sleep(2)
 # # Digitar 2 tabs
 pyautogui.press('tab')
 pyautogui.press('tab')
+pyautogui.press('tab')
 
 # Digitar o número do localizador
-pyautogui.write('FBEXZG')
+# OUFBERGG localizador existente no sistema
+# GUFBEXZG localizador inexistente no sistema
+pyautogui.write('OUFBERGG')
 
 # Pressionar F12
 pyautogui.press('f12')
+
+# Pressionar F5
+pyautogui.press('f5')
+
+# Pressionar control + a
+pyautogui.hotkey('ctrl', 'a')
+
+
+# Limpar a área de transferência
+pyperclip.copy('')
+
+# Adicione uma pausa para garantir que a área de transferência esteja limpa
+time.sleep(1)
+
+# Copiar o texto selecionado para a área de transferência
+pyautogui.hotkey('ctrl', 'c')
+
+# Adicione uma pausa para garantir que o sistema tenha tempo de copiar o texto
+time.sleep(1)
+
+# Obter o texto da área de transferência
+selected_text = pyperclip.paste()
+
+# Verificar se há texto selecionado
+if selected_text:
+  print("localizador não encontrado")
+else:
+  print("localizador encontrado")
+
+
